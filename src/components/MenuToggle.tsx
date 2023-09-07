44d8ebb9b0551e5b2ba8fe7afc710100cd5f9e43
Ronald <RonaldWilsonMachado@gmail.com>
2023-09-07 21:08:26 +0200

    add menu toggle

---
+++ src/components/MenuToggle.tsx
@@ -0,0 +1,28 @@
+import { Menu, Plus } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+   DropdownMenu,
+   DropdownMenuContent,
+   DropdownMenuItem,
+   DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function MenuToggle() {
+
+   return (
+      <DropdownMenu>
+         <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+               <Menu className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+               <span className="sr-only">Toggle Menu</span>
+            </Button>
+         </DropdownMenuTrigger>
+         <DropdownMenuContent align="start">
+            <DropdownMenuItem className="cursor-pointer flex justify-start space-x-2">
+               <Plus className="h-[1.4rem] w-[1.4rem]" />
+               <span>Create Project</span>
+            </DropdownMenuItem>
+         </DropdownMenuContent>
+      </DropdownMenu>
+   )
+}
