970142184205ed3455822440fbd6cbd9ac571174
Ronald <RonaldWilsonMachado@gmail.com>
2023-09-07 22:27:55 +0200

    fix clerk theming

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,15 @@ import './App.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { dark } from '@clerk/themes';
 import { RouterProvider } from "react-router-dom";
 import { router } from './routes.tsx'
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
-
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider appearance={{baseTheme: dark}} publishableKey={'pk_test_c2VsZWN0ZWQtYWtpdGEtODMuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <RouterProvider router={router} />
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,22 @@
-import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { dark } from '@clerk/themes';
 import { Outlet } from "react-router-dom";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+import { useTheme } from '@/components/providers/ThemeProvider';
+
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 export default function Root() {
+
+   const { theme, systemIsDark} = useTheme()
    
    return (
-      <ThemeProvider>
-         <Outlet />
-      </ThemeProvider>
+      <ClerkProvider appearance={{ baseTheme: ((theme === 'dark' || (theme === 'system' && systemIsDark)) ? dark : undefined) }} publishableKey={'pk_test_c2VsZWN0ZWQtYWtpdGEtODMuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+               <Outlet />
+               </ConvexProviderWithClerk>
+         </ClerkProvider>
    )
 }
