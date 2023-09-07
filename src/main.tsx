02c5c8ca43d660d4b439268adf3168c3a71ac0fc
Ronald <RonaldWilsonMachado@gmail.com>
2023-09-07 22:09:55 +0200

    update clerk style

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import './index.css'
+import './App.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { dark } from '@clerk/themes';
 import { RouterProvider } from "react-router-dom";
 import { router } from './routes.tsx'
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
@@ -13,7 +15,7 @@ const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={'pk_test_c2VsZWN0ZWQtYWtpdGEtODMuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
+    <ClerkProvider appearance={{baseTheme: dark}} publishableKey={'pk_test_c2VsZWN0ZWQtYWtpdGEtODMuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <RouterProvider router={router} />
       </ConvexProviderWithClerk>
