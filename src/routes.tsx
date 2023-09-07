97ed6bd8a4ddf8c2855ddb589125b3321479817c
Ronald <RonaldWilsonMachado@gmail.com>
2023-09-07 16:05:22 +0200

    add mui components and setup react router dom

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./routes/Root";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
+export const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Root />,
+      children: [
+         {
+            path: '/',
+            element: <Home />
+         },
+      ]
+   },
+   {
+      path: '/login',
+      element: <Login />
+   }
+])
