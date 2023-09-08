a57d8b58f431241c0e7040578af41e620de5e89f
Ronald <RonaldWilsonMachado@gmail.com>
2023-09-09 00:15:27 +0200

    implement form submission logic
    - Add functionality to handle form submission when creating a new project.
    - Implement logic to call the API endpoint for creating a project.
    - Display a loader while the form is being submitted.
    - Additionally Fetch public repositories for the current user from the Github API.
    - Update the form to display the list of fetched repositories in a select dropdown.

--- src/components/modals/CreateProjectModal.tsx
+++ src/components/modals/CreateProjectModal.tsx
@@ -11,9 +11,17 @@ import { Input } from "@/components/ui/input"
 import { ModalProps } from "@/components/providers/ModalProvider"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+import { useMutation } from "convex/react"
+import { api } from '../../../convex/_generated/api'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { useEffect, useState } from "react"
+import { useUser } from "@clerk/clerk-react"
+import * as github from '@/lib/github';
 import * as z from "zod"
+import { Loader2 } from "lucide-react"
 
 const FormSchema = z.object({
    projectName: z.string().min(4).max(23),
@@ -23,17 +31,45 @@ const FormSchema = z.object({
 type FormType = z.infer<typeof FormSchema>
 
 export function CreateProjectModal({ open, onOpenStateChange }: ModalProps) {
-
+   
+   const createProject = useMutation(api.projects.createProject)
    const form = useForm<FormType>({
       resolver: zodResolver(FormSchema),
       defaultValues: {
          projectName: '',
       }
    })
+   const [isLoading, setLoading] = useState(false)
+
+   // Load public repositories for current user from the github API
+   const [repos, setRepos] = useState<{ name: string, id: number }[]>()
+   const { user } = useUser()
+   useEffect(() => {
+      async function fetchRepos() {
+         if (!user?.username) {
+            return
+         }
+
+         const response = await github.listRepos(user.username);
+         const reposData = response.data.map(repo => ({ name: repo.name, id: repo.id }))
+         setRepos(reposData)
+      }
+
+      fetchRepos()
+   }, [user?.username])
 
    const onSubmit = async (data: FormType, event: React.BaseSyntheticEvent | undefined) => {
       event?.preventDefault()
-      alert(JSON.stringify(data, null, 2))
+      try{
+         setLoading(true)
+         await createProject({name: data.projectName, repo: data.repository})
+      }
+      catch(error){
+         console.error(error)
+      }
+      finally{
+         setLoading(false)
+      }
    }
 
    return (
@@ -62,7 +98,7 @@ export function CreateProjectModal({ open, onOpenStateChange }: ModalProps) {
                      <div className="items-center space-y-2">
                         <FormField name="repository" control={form.control} render={({ field }) => (
                            <FormItem>
-                              <FormLabel>Repository</FormLabel>
+                              <FormLabel>Github Repository</FormLabel>
                               <Select onValueChange={field.onChange} defaultValue={field.value}>
                                  <FormControl>
                                     <SelectTrigger>
@@ -72,15 +108,15 @@ export function CreateProjectModal({ open, onOpenStateChange }: ModalProps) {
 
                                  <SelectContent>
                                     <SelectGroup>
-                                       <SelectLabel className="flex items-center space-x-2">
-                                          <i className="i-mdi-github text-2xl" />
-                                          <span>Public Github Repos</span>
-                                       </SelectLabel>
-                                       <SelectItem value="apple">Apple</SelectItem>
-                                       <SelectItem value="banana">Banana</SelectItem>
-                                       <SelectItem value="blueberry">Blueberry</SelectItem>
-                                       <SelectItem value="grapes">Grapes</SelectItem>
-                                       <SelectItem value="pineapple">Pineapple</SelectItem>
+                                       <ScrollArea className="h-[290px]">
+                                          <SelectLabel className="flex items-center space-x-2">
+                                             <i className="i-mdi-github text-2xl" />
+                                             <span>Public Github Repos</span>
+                                          </SelectLabel>
+                                          {repos?.map((repo) => (
+                                             <SelectItem value={repo.name} key={repo.id}>{repo.name}</SelectItem>
+                                          ))}
+                                       </ScrollArea>
                                     </SelectGroup>
                                  </SelectContent>
                               </Select>
@@ -90,7 +126,10 @@ export function CreateProjectModal({ open, onOpenStateChange }: ModalProps) {
                      </div>
                   </div>
                   <DialogFooter>
-                     <Button type="submit">Create Project</Button>
+                     <Button className="flex items-center space-x-2" disabled={isLoading} type="submit">
+                        <span>Create Project</span>
+                        {isLoading && <Loader2 className="animate-spin" />}
+                     </Button>
                   </DialogFooter>
                </form>
             </Form>
