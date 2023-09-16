ca6a4e444215c82c51ee44ea611933d81ba9d44c
Ronald <RonaldWilsonMachado@gmail.com>
2023-09-16 10:55:03 +0200

    remove console.log statement

--- src/components/providers/StoredUserProvider.tsx
+++ src/components/providers/StoredUserProvider.tsx
@@ -42,7 +42,6 @@ export default function StoredUserProvider({ children }: PropsWithChildren) {
 
    useEffect(() => {
       const intervalId = setInterval(async () => {
-         console.log('Updating last seen')
          await updateLastSeen()
       }, 1 * 60 * 1000)
 
